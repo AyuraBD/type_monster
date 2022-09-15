9f7384fa42ddcebf4b4e6aeade540c23aefcaf22
Mohammad Ayub <5043ayub@gmail.com>
2022-09-15 23:16:04 +0600

    Added responsive and round seconds

--- history.js
+++ history.js
@@ -27,14 +27,18 @@ function displayHistory() {
 
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
-    newRow.classList.add("card");
+    newRow.classList.add("col-md-4");
 
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+      <div class="card-new">
+        <h3>${test.questionText}</h3>
+        <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+        <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+      </div>
   `;
 
     histories.appendChild(newRow);
   });
 }
+
+
--- script.js
+++ script.js
@@ -67,7 +67,8 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeRound = (finishTime - startTime) / 1000;
+  const timeTaken = Math.round(timeRound);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -79,10 +80,12 @@ const gameOver = () => {
   display.classList.add("inactive");
   // show result
   resultModal.innerHTML += `
-    <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
-    <button onclick="closeModal()">Close</button>
+    <div class="finish-container">
+      <h1>Finished!</h1>
+      <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+      <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+      <button onclick="closeModal()">Close</button>
+    </div>
   `;
 
   addHistory(questionText, timeTaken, errorCount);
@@ -110,22 +113,23 @@ const start = () => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
     count--;
+
   }, 1000);
+  
 };
 
 // START Countdown
 startBtn.addEventListener("click", start);
-
 
 // If history exists, show it
 displayHistory();
@@ -134,7 +138,8 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
+  const round = Math.round(timeSpent);
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? round : 0} seconds`;
 }, 1000);
